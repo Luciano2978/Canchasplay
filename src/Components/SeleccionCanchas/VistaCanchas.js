547402522a916bc5b493e6fa2bbab4d8889b1886
Luciano2978 <luciano_2978@hotmail.com>
2023-08-23 18:28:15 -0300

    ajustando responsive

--- src/Components/SeleccionCanchas/VistaCanchas.js
+++ src/Components/SeleccionCanchas/VistaCanchas.js
@@ -11,6 +11,7 @@ import ButtonBase from '@mui/material/ButtonBase';
 import Rating from '@mui/material/Rating';
 import { styled } from '@mui/material/styles';
 import LogoRecova from "../../Assets/Logo_Recova.jpg";
+import { useTheme } from '@mui/material/styles';
 
 
 const Img = styled('img')({
@@ -19,8 +20,39 @@ const Img = styled('img')({
     maxWidth: '55%',
     borderRadius: "60%"
 });
-  
+
+
 export default function VistaCanchas(){
+
+    const theme = useTheme();
+    
+    const PaperStyle = ({
+        p: 2,
+        margin: 'auto',
+        maxWidth: "90% ",
+        maxHeight: "10%",
+        flexGrow: 1,
+        backgroundColor: "rgba(0, 0, 0, 0.77)",
+        borderRadius: "60px",
+        marginBottom: "10px",
+        [theme.breakpoints.down('sm')]: {
+            /* Estilos específicos para pantallas pequeñas */
+            // Por ejemplo:
+            //backgroundColor: "rgba(0, 0, 0, 1)",
+        }
+    });
+
+
+    const LogoStyle = ({
+        width: 128 ,
+        [theme.breakpoints.down('sm')]: {
+            /* Estilos específicos para pantallas pequeñas */
+            // Por ejemplo:
+            backgroundColor: "rgba(0, 0, 0, 0)",
+        }
+    });
+
+
 
     return (
         <>      
@@ -33,28 +65,21 @@ export default function VistaCanchas(){
             }}>
 
                 <Container fixed>
-                    <Box sx={{ bgcolor: 'rgba(52, 52, 52, 0.29)', height: '80vh' }}>
+                    <Box sx={{ bgcolor: 'rgba(52, 52, 52, 0.29)', height: '80vh',padding:"1%" }}>
+                        {/* Aca se debe realizar un map de todas las canchas (dependiendo el deporte) esten disponibles*/}
                         <Paper
-                            sx={{
-                                p: 2,
-                                margin: 'auto',
-                                maxWidth: "90% ",
-                                maxHeight: "10%",
-                                flexGrow: 1,
-                                backgroundColor: "rgba(0, 0, 0, 0.50)",
-                                borderRadius: "5%"
-                            }}
+                            sx={PaperStyle}
                             >
                             <Grid container spacing={2}>
                                 <Grid item>
-                                <ButtonBase sx={{ width: 128 }}>
+                                <ButtonBase sx={LogoStyle}>
                                     <Img alt="complex" src={LogoRecova}  />
                                 </ButtonBase>
                                 </Grid>
                                 <Grid item xs={12} sm container>
                                 <Grid item xs container direction="column">
                                     <Grid item xs>
-                                    <Typography gutterBottom variant="subtitle1" component="div" style={{color:"white"}}>
+                                    <Typography gutterBottom variant="h6" component="div" style={{color:"white"}}>
                                         Cancha 2 LA RECOVA
                                     </Typography>  
                                     </Grid>
@@ -62,19 +87,20 @@ export default function VistaCanchas(){
                                     <Stack spacing={1}>
                                         <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
                                     </Stack>
-                                    <Typography sx={{ cursor: 'pointer' }} variant="body2">
+                                    <Typography sx={{ cursor: 'pointer' }} variant="body2"  style={{color:"white"}}>
                                         3 Comentarios
                                     </Typography>
                                     </Grid>
                                 </Grid>
                                 <Grid item>
-                                    <Typography variant="subtitle1" component="div">
+                                    <Typography variant="h6" component="div" mt={2} sx={{ color: "#44FF02", fontWeight: "bold",fontSize:"25px" }}>
                                     Disponible
                                     </Typography>
                                 </Grid>
                                 </Grid>
                             </Grid>
                         </Paper>
+                        
                     </Box>                    
                 </Container>
             </Box>
