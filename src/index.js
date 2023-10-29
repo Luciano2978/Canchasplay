0edcc42a57162479ab7d39c1ee587a331b7efb00
unknown <luciano_2978@hotmail.com>
2023-10-29 10:29:36 -0300

    arreglando

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
     domain="dev-gonf6ysh.us.auth0.com"
     clientId="vLYBJVhpRwmL86MbveaArcYfBbSqyM3N"
     authorizationParams={{
-      redirect_uri: "https://shk5k0ck-3000.brs.devtunnels.ms/"
+      redirect_uri: "http://localhost:3000/"
     }}
   >
     <App />
