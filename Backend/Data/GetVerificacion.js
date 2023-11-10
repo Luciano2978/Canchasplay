869b9f4485ecc565f19f9200628413bd56977e04
Luciano Rojas <luciano_2978@hotmail.com>
2023-11-10 18:19:07 -0300

    Rutas Protegidas

--- Backend/Data/GetVerificacion.js
+++ Backend/Data/GetVerificacion.js
@@ -6,12 +6,11 @@ const GetVerifiacionProp = (req, res) =>{
 
     console.log(userID)
    
-    const DatosProp = `SELECT p.Verificado, COUNT(*) AS cantidad_de_complejos_relacionados
+    const DatosProp = `SELECT p.Verificado, COUNT(c.id_Complejo) AS cantidad_de_complejos_relacionados
     FROM propietario p
-    JOIN cuenta cu ON p.Cuenta_id_Cuenta = cu.id_Cuenta
     LEFT JOIN complejo c ON p.id_Propietario = c.Propietario_id_Propietario
-    WHERE cu.id_Cuenta = ?
-    GROUP BY p.Verificado`
+    WHERE p.Cuenta_id_Cuenta = ?
+    GROUP BY p.Verificado;`
     // Lógica para eliminar la cancha de la base de datos (consulta SQL de eliminación)
     connection.query(DatosProp, userID , (err,result) => {
         if (err) {
