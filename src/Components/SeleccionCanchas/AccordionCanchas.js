e8f54d313a05028ebeb97803a193137aee355fa5
Luciano Rojas <luciano_2978@hotmail.com>
2023-11-10 16:27:41 -0300

    bug xd

--- src/Components/SeleccionCanchas/AccordionCanchas.js
+++ src/Components/SeleccionCanchas/AccordionCanchas.js
@@ -34,7 +34,7 @@ export default function AccordionCanchas({open,onClose,NombreCancha,idComplejo})
     const dataToSend = {
       idCancha: idCan
     };
-    axios.post("http://localhost:8080/getDataHorarios",dataToSend)
+    axios.post("https://canchas-play.onrender.com/getDataHorarios",dataToSend)
       .then((response ) => {
         setDatosHorarios(response.data);
       })
