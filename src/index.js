a182b168a04b093813823052d54d836d35978a98
Martin Ortega <111008433+MartinOrtega3RL@users.noreply.github.com>
2023-11-15 21:34:36 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
     domain="dev-gonf6ysh.us.auth0.com"
     clientId="vLYBJVhpRwmL86MbveaArcYfBbSqyM3N"
     authorizationParams={{
-      redirect_uri: `http://localhost:3000/Verificacion`  
+      redirect_uri: `https://canchas-playfront.onrender.com/Verificacion`  
     }}
   >
     <App />
